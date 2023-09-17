7b036f6f6f9b77e68bc5bfebbbcbb9335fe2b530
YumiN <yuminishi0309@gmail.com>
2023-09-17 23:52:19 +0900

    add: resize-header

--- js/function.js
+++ js/function.js
@@ -1,8 +1,22 @@
 $(function() {
+    // リサイズヘッダー設定
+    const mvHeight = $('#swiper-mv').height();
 
+    $(window).on('scroll', function(){
+        const ST = $(window).scrollTop();
 
-
-
-
-
+        if(ST >= mvHeight) {
+            $('#header').addClass('resizeHeader');
+            $('.gnav__list').addClass('bgChange');
+            $('.gnav__link').addClass('colorChange');
+            $('.nav__btn').addClass('btnBgChange');
+            $('.nav__link').addClass('btnColorChange');
+        }else {
+            $('#header').removeClass('resizeHeader');
+            $('.gnav__list').removeClass('bgChange');
+            $('.gnav__link').removeClass('colorChange');
+            $('.nav__btn').removeClass('btnBgChange');
+            $('.nav__link').removeClass('btnColorChange');
+        }
+    });
 });
